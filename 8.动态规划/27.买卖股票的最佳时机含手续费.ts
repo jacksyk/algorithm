992cd2be4bf05b3349e435adf46bb192cd84855f
Shu Yi Kang <185901261@qq.com>
2024-07-28 12:17:30 +0800

    chore(master): [build] 7.27

--- 8.动态规划/27.买卖股票的最佳时机含手续费.ts
+++ 8.动态规划/27.买卖股票的最佳时机含手续费.ts
@@ -5,6 +5,7 @@ export {}
 
 // dp[i][0]表示持有股票拥有的最大利润
 // dp[i][1]表示不持有股票拥有的最大利润
+// TODO:探索一下如何debugger，因为目前dp状态越来越复杂，只能手动模拟数值了。然后看对应的。
 function maxProfit(prices: number[], fee: number): number {
     const dp = new Array(prices.length).fill(0).map((_v) => new Array(2).fill(0))
     dp[0][0] = -prices[0]
@@ -13,6 +14,8 @@ function maxProfit(prices: number[], fee: number): number {
         dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i])
         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee)
     }
+    console.table(dp)
+
     return dp[prices.length - 1][1]
 }
 
