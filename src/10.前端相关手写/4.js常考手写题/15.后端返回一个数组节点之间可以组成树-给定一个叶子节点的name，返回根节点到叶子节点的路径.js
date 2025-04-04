1bfb439f3aafd008f0c6f577325a540d9b44d0d8
Shu Yi Kang <185901261@qq.com>
2025-04-05 01:29:57 +0800

    feat: add new algorithm

---
+++ src/10.前端相关手写/4.js常考手写题/15.后端返回一个数组节点之间可以组成树-给定一个叶子节点的name，返回根节点到叶子节点的路径.js
@@ -0,0 +1,38 @@
+const data = [
+    { id: '01', name: 'Root', pid: '' },
+    { id: '02', name: 'DirA', pid: '01' },
+    { id: '03', name: 'DirB', pid: '01' },
+    { id: '04', name: 'File1.txt', pid: '02' },
+    { id: '05', name: 'File2.txt', pid: '03' }
+];
+
+const getPathByLeafName = (data, leafName) => {
+    // 找到叶子节点，获取对应的pid，往上递归寻找就行
+    const map = new Map()
+
+    data.forEach(item => {
+        map.set(item.id, item)
+    })
+
+    const path = []
+
+    const findPath = (leafName) => {
+        const node = data.find(item => item.name === leafName)
+        if (node) {
+            path.unshift(node.name)
+            if (node.pid) {
+                findPath(map.get(node.pid).name)
+            }
+        }
+    }
+    findPath(leafName)
+    return path.join('/')
+}
+
+
+
+
+console.log(getPathByLeafName(data, 'File1.txt'));
+// 输出: ['Root/DirA/File1.txt']
+console.log(getPathByLeafName(data, 'File2.txt'));
+// 输出: ['Root/DirB/File2.txt']
---
+++ src/10.前端相关手写/4.js常考手写题/16.hardman.js
@@ -0,0 +1,94 @@
+/**
+ * @description 腾讯爱考题目-来自牛客
+ * @url https://juejin.cn/post/7406150677225898023
+ */
+
+// ps: 关键设计思路（针对于restFirst函数）
+// 1.任务队列机制
+// 2.同步任务和异步任务的方式。异步执行控制
+
+// 写一个hardMan函数，满足控制台打印效果如下：
+// ===============================================
+// hardMan('潘潘')
+//> Hi! I am 潘潘.
+// =============================================== 
+// hardMan('潘潘').study('Project')
+//> Hi! I am 潘潘.
+//> I am studying 敲码.
+// ===============================================
+// hardMan('潘潘').rest(3).study('敲码')
+//> Hi! I am 潘潘.
+// 此时等待三秒钟
+//> Wait 3 seconds.
+//> I am studying 敲码.
+// ===============================================
+// hardMan('潘潘').restFirst(3).study('敲码')
+// 此时等待三秒钟
+//> Wait 3 seconds.
+//> Hi! I am 潘潘.
+//> I am studying 敲码.
+// ===============================================
+
+async function sleep(seconds) {
+    await new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`Wait ${seconds} seconds.`);
+            resolve()
+        }, seconds * 1000);
+    })
+}
+
+
+function hardManClass(name) {
+    this.task = []
+
+    setTimeout(async () => {
+        for (let tak of this.task) {
+            await tak()
+        }
+    }, 0);
+
+
+    this.task.push(() => {
+        return new Promise(resolve => {
+            console.log(`Hi! I am ${name}.`);
+            resolve()
+        })
+    })
+
+    this.study = function (studyName) {
+        this.task.push(() => {
+            return new Promise(resolve => {
+                console.log(`I am studying ${studyName}.`);
+                resolve()
+            })
+        })
+        return this;
+    }
+
+    this.rest = function (time) {
+        this.task.push(async () => {
+            await sleep(time)
+        })
+        return this
+    }
+
+    this.restFirst = function (time) {
+        this.task.unshift(async () => {
+            await sleep(time)
+        })
+        return this
+    }
+
+    return this
+}
+
+function hardMan(name) {
+    return new hardManClass(name)
+}
+
+
+// hardMan('潘潘')
+// hardMan('潘潘').study('Project')
+hardMan('潘潘').rest(3).study('敲码')
+// hardMan('潘潘').restFirst(3).study('敲码')
